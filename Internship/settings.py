f304637c764a3847a10ab8495d9ad776b8c96670
Nattanan Nawakitbamrung <6210612567@cn330>
2022-03-20 01:56:22 +0700

    Edit staticfile

--- Internship/settings.py
+++ Internship/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from email.mime import base
 from pathlib import Path
 import os
 
@@ -124,9 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [BASE_DIR / "static", ]
 
 
 # Default primary key field type

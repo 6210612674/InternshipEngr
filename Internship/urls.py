006728a12c58073b7d56e98599cf735d701819eb
Pattana Sapphanachai <6210612831@student.tu.ac.th>
2022-03-21 23:53:04 +0700

    Thread Backend(create,search,update,delete) and update internship's urls

--- Internship/urls.py
+++ Internship/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include("account.urls")),
+    #path('account/', include("account.urls")),
+    path('', include("account.urls")),
+    path('thread/', include("thread.urls")),
+    url(r'^markdownx/', include('markdownx.urls')),
 ]
--- thread/urls.py
+++ thread/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from . import views
 
 app_name = 'thread'
-urlpatterns = [path("", views.index, name="index")
-
+urlpatterns = [path("", views.index, name="index"),
+               path("testmd", views.testmd, name="testmd"),
+               path("create_thread", views.create_thread, name="create_thread"),
+               path("<int:thread_id>", views.thread, name="thread"),
+               path("delete_thread/<int:thread_id>",
+                    views.delete_thread, name="delete_thread"),
+               path("update/<int:thread_id>",
+                    views.update_thread, name="update_thread"),
                ]
